9a82a3798083e0d77fa74946cc18513849d3cc4d
guoyff <guoyff@yonyou.com>
2017-07-27 19:42:45 +0800

    update filename

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-import {
-    compMgr
-} from './compMgr';
+import { compMgr } from './compMgr';
 import {
     App,
     createApp
 } from './createApp';
-import { compoxUtil } from  './app/index.js';
-import {
-    use,
-    Component
-} from './expand.js';
+// import { compoxUtil } from  './app/index.js';
+import use from './use.js';
+import Component from './Component.js'
 
-use(compoxUtil);
+// use(compoxUtil);
 
 /**
  * createApp() : 创建应用服务
  * use() : 使用 plugin 或 middleware
  * createModel() : 创建数据模型
  */
-export {
-    App,
-    createApp,
-    compMgr,
-    use,
-    Component
+export default {
+  // App,
+  createApp,
+  compMgr,
+  use,
+  Component
 };
