39c7c6c170d6409645aecd714d7b65ddfe6c6b8d
huyueb <1062887235@qq.com>
2017-07-27 12:22:35 +0800

    将原有的项目内容迁移过来

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from "rollup-plugin-babel";
+import multidest from "rollup-plugin-multidest";
+import nodeResolve from "rollup-plugin-node-resolve";
+import uglify from "rollup-plugin-uglify";
+import license from "rollup-plugin-license";
+// import package from './package.json';
+
+export default {
+    entry: "src/index.js",
+    dest: `dist/moy.js`,
+    format: "iife",
+    moduleName: "bar",
+    plugins: [
+        license({
+            banner: `<%= pkg.name %> v<%= pkg.version %>
+                    <%= pkg.description %>
+                    author : <%= pkg.author %>
+                    homepage : <%= pkg.homepage %>
+                    bugs : <%= pkg.bugs.url %>`,
+        }),
+        nodeResolve(),
+        multidest([
+            {
+                dest: `dist/moy.min.js`,
+                format: "iife",
+                plugins: [uglify()]
+            }
+        ]),
+        babel({
+            // exclude: "node_modules/**" // only transpile our source code
+        })
+    ]
+};
